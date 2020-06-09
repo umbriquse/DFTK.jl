f74da78e84b08b702ae5fdf06e0a0e5b99b151fe
Gaspard Kemlin <gaspard.kemlin@enpc.fr>
2020-06-09 17:39:16 +0200

    interpolate blochwave for tuning optimization

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -9,7 +9,7 @@ function interpolate_density(ρ_in::RealFourierArray, b_out::PlaneWaveBasis)
     from_real(b_out, ρ_out)
 end
 
-# TODO Specialization for the common case lattice_out = lattice_in
+# TODO Specialisation for the common case lattice_out = lattice_in
 function interpolate_density(ρ_in::AbstractArray, grid_in, grid_out, lattice_in, lattice_out=lattice_in)
     T = real(eltype(ρ_in))
     @assert size(ρ_in) == grid_in
@@ -64,7 +64,7 @@ end
 
 """
 Interpolate some data from one k-Point to another. The interpolation is fast, but not
-necessarily exact or even normalized. Intended only to construct guesses for iterative
+necessarily exact or even normalised. Intended only to construct guesses for iterative
 solvers
 """
 function interpolate_kpoint(data_in::AbstractVecOrMat, kpoint_in::Kpoint, kpoint_out::Kpoint)
@@ -84,37 +84,82 @@ function interpolate_kpoint(data_in::AbstractVecOrMat, kpoint_in::Kpoint, kpoint
     data_out
 end
 
+"""
+Compute indices from basis_in that are in basis_out, alongside the complementary
+set of indices of basis_out that are not in basis_in. Currently only for basis_out
+bigger than basis_in.
+"""
+function grid_interpolation_indices(basis_in, basis_out)
+    # build the indices from basis_out that are in basis_in
+    idcs_out = []
+    for (ik, kpt_out) in enumerate(basis_out.kpoints)
+        # Get indices of the G vectors of the old basis inside the new basis.
+        idcsk_out = index_G_vectors.(Ref(basis_out), G_vectors(basis_in.kpoints[ik]))
+
+        # Linearise the indices
+        idcsk_out = getindex.(Ref(LinearIndices(basis_out.fft_size)), idcsk_out)
+
+        # Map to the indices of the corresponding G-vectors in G_vectors(kpt_out)
+        idcsk_out = indexin(idcsk_out, kpt_out.mapping)
+        # this array might contains some nothings if basis_out has less G_vectors
+        # than basis_in at this kpoint
+        push!(idcs_out, idcsk_out)
+    end
+
+    # build the complement indices of basis_out that are not in basis_in
+    # create empty arrays if there is at least one nothing in idcs_out[ik]
+    idcs_out_cplmt = [[id for id in 1:length(G_vectors(basis_out.kpoints[ik]))
+                       if any(isnothing, idcs_out[ik]) && !(id in idcs_out[ik])]
+                      for ik in 1:length(basis_in.kpoints)]
+
+    (idcs_out, idcs_out_cplmt)
+end
 
 """
 Interpolate Bloch wave between two basis sets. Limited feature set. Currently only
 interpolation to a bigger grid (larger Ecut) on the same lattice supported.
 """
 function interpolate_blochwave(ψ_in, basis_in, basis_out)
-    @assert basis_in.model.lattice == basis_out.model.lattice
     @assert length(basis_in.kpoints) == length(basis_out.kpoints)
     @assert all(basis_in.kpoints[ik].coordinate == basis_out.kpoints[ik].coordinate
                 for ik in 1:length(basis_in.kpoints))
 
     ψ_out = empty(ψ_in)
+
+    # separating indices from basis_out that are in basis_in from those that are
+    # not in basis_in, eventually idcs_out[ik] contains nothings, in which case
+    # idcs_out_cplmt[ik] = [] when the output grid is smaller
+    idcs_out, idcs_out_cplmt = grid_interpolation_indices(basis_in, basis_out)
+
+    # separating indices from basis_in that are in basis_out from those that are
+    # not in basis_out, eventually idcs_in[ik] contains nothing, in which case
+    # idcs_in_cplmt[ik] = [] when the output grid is bigger
+    idcs_in, idcs_in_cplmt = grid_interpolation_indices(basis_out, basis_in)
+
+    # Small talk on what we did here :
+    # When Ecut_out > Ecut_in, only idcs_out is needed. Sometimes, we might need
+    # to interpolate between two grids that have the same Ecut (hence same
+    # fft_size) but slightly different lattices. In this case, both idcs_out and
+    # idcs_in are needed as it is not possible to predict which basis will have most
+    # G_vectors (and it might differs from the k points !). This is why we do
+    # both interpolations by default, and use the interpolation of basis_out into
+    # basis_in when needed.
+
     for (ik, kpt_out) in enumerate(basis_out.kpoints)
         n_bands = size(ψ_in[ik], 2)
-
-        # Get indices of the G vectors of the old basis inside the new basis.
-        idcs_out = index_G_vectors.(Ref(basis_out), G_vectors(basis_in.kpoints[ik]))
-
-        # Linearize the indices
-        idcs_out = getindex.(Ref(LinearIndices(basis_out.fft_size)), idcs_out)
-
-        # Map to the indices of the corresponding G-vectors in G_vectors(kpt_out)
-        idcs_kpt_out = indexin(idcs_out, kpt_out.mapping)
-        @assert !any(isnothing, idcs_kpt_out)
 
         # Set values
         ψk_out = similar(ψ_in[ik], length(G_vectors(kpt_out)), n_bands)
         ψk_out .= 0
-        ψk_out[idcs_kpt_out, :] .= ψ_in[ik]
+        if !any(isnothing, idcs_out[ik])
+            # only this is used when Ecut_out > Ecut_in
+            ψk_out[idcs_out[ik], :] .= ψ_in[ik]
+        else
+            # when needed, we get rid of high frequencies G_vectors
+            ψk_out .= ψ_in[ik][idcs_in[ik], :]
+        end
         push!(ψ_out, ψk_out)
     end
 
-    ψ_out
+    ψ_out, idcs_out, idcs_out_cplmt
 end
